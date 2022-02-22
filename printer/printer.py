62bdd8e79771650d36968db5be45df52b77956f0
Justin <jyl20@ic.ac.uk>
2022-02-22 23:06:47 +0000

    done

--- printer/printer.py
+++ printer/printer.py
@@ -3,9 +3,38 @@ from collections import deque
 
 
 class Document:
-    def __init__(self, strings=(,)):
+    def __init__(self, strings=[]):
         self.queue = deque(strings)
-    
-    def append(self, string):
-        self.queue.append()
 
+    def append(self, string):
+        self.queue.append(string)
+
+    def print(self):
+        return self.queue.popleft()
+    
+    def __len__(self):
+        return len(self.queue)
+
+
+class Printer:
+    def __init__(self):
+        self.queue = deque()
+    
+    def enqueue(self, doc):
+        self.queue.append(doc)
+
+    def cancel(self):
+        self.queue.popleft()
+
+    def __len__(self):
+        return len(self.queue)
+
+    def pages(self):
+        return sum([len(doc) for doc in self.queue])
+
+    def print(self):
+        first_doc= self.queue[0]
+        page = first_doc.print()
+        if not first_doc:
+            self.queue.popleft()
+        return page
